ad6bbb01f0bdc5723ce8e1149d823bbaf1e4a16f
Vismyfriend <vismyfriend@yahoo.com>
2023-05-02 20:58:50 +0300

    One Deck Ready

--- scripts/app.js
+++ scripts/app.js
@@ -8,29 +8,56 @@ const manual1 = document.querySelector(".manual-1")
 const manual2 = document.querySelector(".manual-2")
 const manual3 = document.querySelector(".manual-3")
 const hint = document.querySelector(".notebook__hint")
-let randomNumber = null
-let min = null
-let max = null
 let value = null
-function getAnswers() {
+let questionNumber = 0
+let answerNumber = 0
+
+
+let shuffledQuestions = null
+
+let shuffledAnswers = null
+
+
+function shuffleDecks() {
+    shuffledQuestions = questions.sort(function () {
+        return Math.random() - 0.5;
+    });
+    shuffledAnswers = answers.sort(function () {
+        return Math.random() - 0.5;
+    });
+    questionNumber= 0
+    answerNumber = 0
+    askMe.classList.remove("finish")
+    askMe.disabled = false
+    next.classList.remove("finish")
+    next.disabled = false
     card.classList.remove("open")
-    showCheckManual()
-    min = answers[0].id
-    max = min + answers.length
-    setRandomNumber()
-    console.log(randomNumber)
-    value = answers.find(n => n.id === randomNumber)
-    cardHint.classList.remove("show")
-    // ищет объект в массиве и сравнивает с id
-    setTimeout(function () { card.classList.add("open") }, 0);
-    card.style.border = "solid 4px rgb(235, 70, 70)";
-    cardValue.textContent = value.eng
-    cardHint.textContent = value.ru
-}
-function setRandomNumber() {
-    randomNumber = Math.floor(Math.random() * (max - min) + min)
+    card.style.border = 'none';
 
 }
+
+function getAnswers() {
+    if (answerNumber < shuffledAnswers.length) {
+
+
+        card.classList.remove("open")
+        value = shuffledAnswers[answerNumber]
+
+        cardHint.classList.remove("show")
+        // ищет объект в массиве и сравнивает с id
+        setTimeout(function () { card.classList.add("open") }, 0);
+        card.style.border = "solid 4px rgb(235, 70, 70)";
+        cardValue.textContent = value.eng
+        cardHint.textContent = value.ru
+        answerNumber++
+    } else {
+        askMe.classList.add("finish")
+        // foo.setAttribute('disabled', 'disabled')
+        askMe.disabled = true
+    }
+
+}
+
 function showQuestionsManual() {
 
 }
@@ -42,29 +69,34 @@ function showCheckManual() {
 
 // глагол на первом месте
 function getquestions() {
-    min = questions[0].id
-    max = min + questions.length
-    card.classList.remove("open")
-    setRandomNumber()
-    setTimeout(function () { card.classList.add("open") }, 0);
-    value = questions.find(n => n.id === randomNumber)
-    cardHint.classList.remove("show")
-    // обновляет blur на каждой новой карточке
-    cardValue.textContent = value.eng
-    cardHint.textContent = value.ru
-    card.style.border = "solid 4px rgb(5, 184, 5)";
-    showQuestionsManual()
+    if (questionNumber < shuffledQuestions.length) {
+        card.classList.remove("open")
+
+        setTimeout(function () { card.classList.add("open") }, 0);
+        value = shuffledQuestions[questionNumber]
+        cardHint.classList.remove("show")
+        // обновляет blur на каждой новой карточке
+        cardValue.textContent = value.eng
+        cardHint.textContent = value.ru
+        card.style.border = "solid 4px rgb(5, 184, 5)";
+        questionNumber++
+    } else {
+        next.classList.add("finish")
+        // foo.setAttribute('disabled', 'disabled')
+        next.disabled = true
+    }
 }
 
 
+shuffleDecks ()
+// сразу перемешивание при входе 
 
-
-check.addEventListener("click", () => {
+cardHint.addEventListener("click", () => {
     cardHint.classList.toggle("show")
 })
 
 askMe.addEventListener("click", getAnswers)
-
+check.addEventListener("click", shuffleDecks)
 next.addEventListener("click", getquestions)
 card.addEventListener("click", () => {
     card.classList.contains("open") ? null : getquestions()
